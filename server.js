885123cf5e48871145774a53aab4dc948a668c29
theoryofchao <roger.ti.chao@gmail.com>
2017-02-08 16:45:00 -0500

    finished project without stretch

--- server.js
+++ server.js
@@ -15,27 +15,60 @@ const server = express()
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
+const colors = ['#0000ff', '#009933', '#ff0000', '#ff00ff'];  //blue green red fuschia
+let numUsers = 0;
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
+  numUsers++;
+  console.log(numUsers);
+
+
   console.log('Client connected');
+
+  const assignedColor = colors[numUsers%4];
+  console.log(assignedColor);
+  ws.send(JSON.stringify({type: "color", color: assignedColor}));
+
+  //sending numUsers for the first time
+  wss.clients.forEach(function each(client) {
+    client.send(JSON.stringify({type: "numUsers", numUsers: numUsers}));
+  });
+
 
   ws.on('message', function incoming(message) {
     let newMessage = JSON.parse(message);
     newMessage.id = uuid.v1();
 
-    wss.clients.forEach(function each(client) {
-      console.log("Sending Message",JSON.stringify(newMessage));
-        client.send(JSON.stringify(newMessage));
-      /*if(client !== ws && client.readyState === WebSocket.OPEN) {
+    switch(newMessage.type) {
+      case 'postMessage':
+        newMessage.type = 'incomingMessage';
+        break;
 
-      }*/
+      case 'postNotification':
+        newMessage.type = 'incomingNotification';
+        break;
+
+
+    };
+
+    wss.clients.forEach(function each(client) {
+      console.log("Sending Message",JSON.stringify({type: "numUsers", numUsers: numUsers}));
+      client.send(JSON.stringify(newMessage));
     });
 
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    numUsers--;
+
+    wss.clients.forEach(function each(client) {
+      console.log("Sending Message",JSON.stringify({type: "numUsers", numUsers: numUsers}));
+      client.send(JSON.stringify({type: "numUsers", numUsers: numUsers}));
+    });
+  });
 });
